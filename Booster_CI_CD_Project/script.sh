e357d5532086ac7baf4a803f77307513a09efd55
AhmedArafat <63191956+Ahmed-Arafat10@users.noreply.github.com>
2021-10-03 13:27:45 +0200

    Create script.sh

---
+++ Booster_CI_CD_Project/script.sh
@@ -0,0 +1,5 @@
+// This script is used to save Private Key of container slave in your host {will be used in credential menu in jenkins server} 
+// then copy file that containes IPAddress of container slave {will be used for host option while creating new node}
+#! bin/bash
+docker cp $1:/root/.ssh/id_rsa /home/arafat/Desktop/jenkinsproject/PrivateKey
+docker inspect $1 > IPAddress
